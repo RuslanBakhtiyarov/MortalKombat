e7e43ef634bafeb0c55fb399203ba37521d54dd0
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2021-10-16 00:12:49 +0300

    10

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const $arenas = document.querySelector('.arenas')
+const randomButton = document.querySelector('.button')
 const player1 = {
     player:1,
     name: 'SCORPION',
-    hr: 80,
+    hr: 100,
     img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
     weapon: ['nunchucks', 'Fire sword', 'stick killer'],
     attack: function () {
@@ -12,7 +13,7 @@ const player1 = {
 const player2 = {
     player:2,
     name: 'SUB-ZERO',
-    hr: 50,
+    hr: 80,
     img: "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
     weapon: ['nunchucks', 'Fire sword', 'stick killer'],
     attack: function () {
@@ -37,7 +38,6 @@ function createPlayer(playerObj) {
     const $name = createElement('div','name');
     const $img = createElement('img');
   
-  
     $life.style.width = playerObj.hr + "%";
     $name.innerText = playerObj.name;
     $img.src = playerObj.img;
@@ -51,7 +51,31 @@ function createPlayer(playerObj) {
   return $player;
 
 };
-$arenas.appendChild(createPlayer(player1))
-$arenas.appendChild(createPlayer(player2))
+
+function hancheHP(player) {
+  const $playerLife = document.querySelector('.player' + player.player + ' .life');
+  player.hr -= 20;
+  $playerLife.style.width = player.hr + '%';
+
+  if (player.hr <= 0) {
+    $arenas.appendChild(playerLose(player.name));
+  
+  }
+}
+  function playerLose(name) {
+    const loseTitle = createElement('div', 'loseTitle');
+    loseTitle.innerText = name + ' lose';
+
+    return loseTitle;
+  }
+  randomButton.addEventListener('click', function () {
+    console.log('####:Click Random button');
+    hancheHP(player1);
+    hancheHP(player2);
+  
+  })
+
+  $arenas.appendChild(createPlayer(player1))
+  $arenas.appendChild(createPlayer(player2))
 
 
